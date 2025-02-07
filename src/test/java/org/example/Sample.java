e3f9a1b1b278f35aa1389915fd30a1d6dea323ae
Danny2727 <dannyedouard14@gmail.com>
2025-02-06 21:05:08 -0500

    New Project

---
+++ src/test/java/org/example/Sample.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jack!!!");
+        System.out.println("Making a change");
+    }
+}
